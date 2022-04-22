a655216edb747b19d7797e27c3ff298818aff64c
jura441 <102172272+jura441@users.noreply.github.com>
2022-04-22 19:57:34 +0300

    TicTacToe

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,6 +1,46 @@
 #include<iostream>
 using namespace std;
 
+void elevator(int floor);
+double power(double a, int n);
+int factorial(int n);
+int fibbonacci(int n);
+//#define ELEVATOR
+//#define FACTORIAL
+//#define POWER 
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	/*cout << "Hello World!";
+	main();*/
+	
+#ifdef ELEVATOR
+	double a;
+	int n;
+	cout << "Введите номер этажа: "; cin >> n;
+	elevator(n);
+#endif // ELEVATOR
+	
+#ifdef FACTORIAL
+	int n;
+	cout << "Введите число для вычисления Факториала: "; cin >> n;
+	cout << factorial(n) << endl;
+#endif // FACTORIAL
+
+#ifdef POWER
+	double a;
+	int n;
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> n;
+	cout << power(a, n) << endl;
+#endif // POWER
+	 
+	int n;
+	cout << "Введите количество чисел Фибоначчи: "; cin >> n;
+	cout << fibbonacci(n) << endl;
+}
+
 void elevator(int floor)
 {
 	if (floor == 0)
@@ -9,17 +49,31 @@ void elevator(int floor)
 		return;
 	}
 	cout << "Вы на " << floor << " этаже\n";
-	elevator(floor-1);
+	elevator(floor - 1);
 	cout << "Вы на " << floor << " этаже\n";
 }
 
-void main()
+int factorial(int n)
 {
-	setlocale(LC_ALL, "");
-	/*cout << "Hello World!";
-	main();*/
+	if (n < 0)return 0;
+	if (n == 0)return 1;
+	return n* factorial(n - 1);
+}
 
-	int n;
-	cout << "Введите номер этажа: "; cin >> n;
-	elevator(n);
+double power(double a, int n)
+{
+	/*if (n == 0) return 1;
+	else if (n > 0) return a * power(a, n - 1);
+	else return 1 / a * power(a, n + 1);
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / a * power(a, n + 1);*/
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / power(a, -n);
+
+}
+
+int fibbonacci(int n)
+{
+	if (n == 0)return 0;
+	if (n == 1)return 1;
+	return fibbonacci(n - 1) + fibbonacci(n - 2);
+	return n == 0 ? 0 : n == 1 ? 1 : fibbonacci(n - 1) + fibbonacci(n - 2);
 }
